b70112776732d1c5e8f7a49085f5e2d43332c84e
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-05-25 12:13:43 -0300

    create comment repository

---
+++ src/main/java/com/github/riannegreiros/blogapi/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.github.riannegreiros.blogapi.repository;
+
+import com.github.riannegreiros.blogapi.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
