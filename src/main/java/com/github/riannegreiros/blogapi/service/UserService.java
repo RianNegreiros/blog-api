d5466092a09d41e9728210f28813f7e5076d6ad7
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-05-30 10:54:23 -0300

    create user service

--- src/main/java/com/github/riannegreiros/blogapi/service/UserService.java
+++ src/main/java/com/github/riannegreiros/blogapi/service/UserService.java
@@ -1,12 +1,12 @@
 package com.github.riannegreiros.blogapi.service;
 
 import com.github.riannegreiros.blogapi.dto.CreateUserRoleDTO;
+import com.github.riannegreiros.blogapi.dto.SignUpDTO;
 import com.github.riannegreiros.blogapi.entity.User;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface UserService {
+    User createUser(SignUpDTO signUpDTO);
+    User findUserByUsername(String username);
 
     User createUserRole(CreateUserRoleDTO createUserRoleDTO);
-    User createUser(User user);
 }
